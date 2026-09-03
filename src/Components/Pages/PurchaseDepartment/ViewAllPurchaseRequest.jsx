f57eb70383b397db5352f7c3102e87064400f871
tausif900 <ansaritausif9268@gmail.com>
2026-09-03 12:22:59 +0530

    purchase request is in progress

--- src/Components/Pages/PurchaseDepartment/ViewAllPurchaseRequest.jsx
+++ src/Components/Pages/PurchaseDepartment/ViewAllPurchaseRequest.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { api } from "../../api";
 
 const ViewAllPurchaseRequest = () => {
+  const [purchaseRequest, setPurchaseRequest] = useState([]);
   const navigate = useNavigate();
+
+  const fetchPendingPurchaseRequest = async () => {
+    try {
+      const response = await api.get("/purchase-request");
+      console.log(response.data);
+      setPurchaseRequest(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPendingPurchaseRequest();
+  }, []);
+
   return (
     <div
       style={{
@@ -39,120 +56,158 @@ const ViewAllPurchaseRequest = () => {
         </button>
       </div>
       {/* Table Container */}
-      <div
-        style={{
-          backgroundColor: "#ffffff",
-          borderRadius: "18px",
-          padding: "20px",
-          border: "1px solid #e8c18f",
-          boxShadow: "0 8px 25px rgba(107,50,5,0.08)",
-          overflowX: "auto",
-        }}
-      >
-        <table
-          className="table align-middle mb-0"
-          style={{ minWidth: "950px" }}
-        >
-          <thead>
-            <tr
-              style={{
-                background: "linear-gradient(135deg, #fff3df, #ffe1b8)",
-              }}
+      {purchaseRequest.length > 0 ? (
+        <>
+          {" "}
+          <div
+            style={{
+              backgroundColor: "#ffffff",
+              borderRadius: "18px",
+              padding: "20px",
+              border: "1px solid #e8c18f",
+              boxShadow: "0 8px 25px rgba(107,50,5,0.08)",
+              overflowX: "auto",
+            }}
+          >
+            <table
+              className="table align-middle mb-0"
+              style={{ minWidth: "950px" }}
             >
-              <th
-                style={{
-                  color: "#8a4b08",
-                  padding: "16px",
-                  borderBottom: "2px solid #e8c18f",
-                }}
-              >
-                Product
-              </th>
-              <th
-                style={{
-                  color: "#8a4b08",
-                  padding: "16px",
-                  borderBottom: "2px solid #e8c18f",
-                }}
-              >
-                Quantity
-              </th>
-              <th
-                style={{
-                  color: "#8a4b08",
-                  padding: "16px",
-                  borderBottom: "2px solid #e8c18f",
-                }}
-              >
-                Requested By
-              </th>
-              <th
-                style={{
-                  color: "#8a4b08",
-                  padding: "16px",
-                  borderBottom: "2px solid #e8c18f",
-                }}
-              >
-                Request Date
-              </th>
-              <th
-                style={{
-                  color: "#8a4b08",
-                  padding: "16px",
-                  borderBottom: "2px solid #e8c18f",
-                }}
-              >
-                Status
-              </th>
-              <th
-                style={{
-                  color: "#8a4b08",
-                  padding: "16px",
-                  borderBottom: "2px solid #e8c18f",
-                  textAlign: "center",
-                }}
-              >
-                Action
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* Row 1 */}
-            <tr>
-              <td style={{ color: "#8a4b08", fontWeight: "700" }}>
-                <i className="bi bi-box-seam me-2"></i> Oil Filter
-              </td>
-              <td style={{ color: "#8a4b08", fontWeight: "600" }}>20 Units</td>
-              <td style={{ color: "#8a4b08", fontWeight: "600" }}>
-                Stock Department
-              </td>
-              <td style={{ color: "#8a4b08", fontWeight: "600" }}>
-                28 Aug 2026
-              </td>
-              <td>
-                <span
+              <thead>
+                <tr
                   style={{
-                    backgroundColor: "#fff3cd",
-                    color: "#b45309",
-                    padding: "7px 14px",
-                    borderRadius: "20px",
-                    fontSize: "12px",
-                    fontWeight: "700",
+                    background: "linear-gradient(135deg, #fff3df, #ffe1b8)",
                   }}
                 >
-                  Pending
-                </span>
-              </td>
-              <td className="text-center">
-                <button className="btn btn-success">
-                  <i className="bi bi-arrow-right-circle me-1"></i> Approve
-                </button>
-              </td>
-            </tr>
-            {/* Row 2 */}
-          </tbody>
-        </table>
-      </div>
+                  <th
+                    style={{
+                      color: "#8a4b08",
+                      padding: "16px",
+                      borderBottom: "2px solid #e8c18f",
+                    }}
+                  >
+                    Product
+                  </th>
+                  <th
+                    style={{
+                      color: "#8a4b08",
+                      padding: "16px",
+                      borderBottom: "2px solid #e8c18f",
+                      textAlign: "center",
+                    }}
+                  >
+                    Current Quantity
+                  </th>
+                  <th
+                    style={{
+                      color: "#8a4b08",
+                      padding: "16px",
+                      borderBottom: "2px solid #e8c18f",
+                      textAlign: "center",
+                    }}
+                  >
+                    Requested Quantity
+                  </th>
+                  <th
+                    style={{
+                      color: "#8a4b08",
+                      padding: "16px",
+                      borderBottom: "2px solid #e8c18f",
+                    }}
+                  >
+                    Requested By
+                  </th>
+                  <th
+                    style={{
+                      color: "#8a4b08",
+                      padding: "16px",
+                      borderBottom: "2px solid #e8c18f",
+                    }}
+                  >
+                    Request Date
+                  </th>
+                  <th
+                    style={{
+                      color: "#8a4b08",
+                      padding: "16px",
+                      borderBottom: "2px solid #e8c18f",
+                    }}
+                  >
+                    Status
+                  </th>
+                  <th
+                    style={{
+                      color: "#8a4b08",
+                      padding: "16px",
+                      borderBottom: "2px solid #e8c18f",
+                      textAlign: "center",
+                    }}
+                  >
+                    Action
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {purchaseRequest.map((p) => {
+                  return (
+                    <tr>
+                      <td style={{ color: "#8a4b08", fontWeight: "700" }}>
+                        <i className="bi bi-box-seam me-2"></i> {p.productName}
+                      </td>
+                      <td
+                        style={{
+                          color: "#8a4b08",
+                          fontWeight: "600",
+                          textAlign: "center",
+                        }}
+                      >
+                        {p.currentQuantity} Units
+                      </td>
+                      <td
+                        style={{
+                          color: "#8a4b08",
+                          fontWeight: "600",
+                          textAlign: "center",
+                        }}
+                      >
+                        {p.requestedQuantity} Units
+                      </td>
+                      <td style={{ color: "#8a4b08", fontWeight: "600" }}>
+                        Stock Department
+                      </td>
+                      <td style={{ color: "#8a4b08", fontWeight: "600" }}>
+                        {p.requestDate}
+                      </td>
+                      <td>
+                        <span
+                          style={{
+                            backgroundColor: "#fff3cd",
+                            color: "#b45309",
+                            padding: "7px 14px",
+                            borderRadius: "20px",
+                            fontSize: "12px",
+                            fontWeight: "700",
+                          }}
+                        >
+                          {p.status}
+                        </span>
+                      </td>
+                      <td className="text-center">
+                        <button className="btn btn-success">
+                          <i className="bi bi-arrow-right-circle me-1"></i>{" "}
+                          Approve
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </>
+      ) : (
+        <div className="text-center">No Pending Request Found</div>
+      )}
     </div>
   );
 };
